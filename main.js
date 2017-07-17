0b648bfc1f9370e38836821a7fe740dbbf14bc64
Ryan Lee <djrlee51@gmail.com>
2017-07-17 15:20:57 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+let container = document.querySelector('#container')
+
+let req = new XMLHttpRequest()
+req.addEventListener("load", displayInfo)
+req.open('GET', 'https://api.github.com/users/Rylee9951')
+req.send()
+
+function displayInfo(){
+	let item = JSON.parse(this.responseText)
+	let info = ''
+
+		info += `
+			<header class="head">${item.name}</header>
+			<div class="columnContianer">
+				<div class="leftColumn">
+					<h2>The Basics</h2>
+					<ul>
+						<li>Name: ${item.name}</li>
+						<li>Github URL: ${item.html_url}</li>
+						<li>Email: ${item.email}</li>
+						<li>Company: ${item.company}</li>
+						<li>Website: ${item.blog}</li>
+					</ul>
+				</div>
+				<div class="centerColumn">
+					<h2>The Story</h2>
+					<p>${item.bio}</p>
+				</div>
+				<div class="rightColumn">
+					<img src="${item.avatar_url}"
+				</div>
+			</div>
+		`	
+	container.innerHTML = info
+}
+
